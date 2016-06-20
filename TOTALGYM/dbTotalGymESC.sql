f443c1121610f8c71b4ee46f162d1b8335638509
Horacio Enrique Castillo Puente <horacio.castillopnt@gmail.com>
2016-06-20 10:10:41 -0500

    Se agrego la carpeta para agregar los scripts de la base de datos y el script inicial

---
+++ TOTALGYM/dbTotalGymESC.sql
@@ -0,0 +1,11 @@
+﻿/*************************************************************************************************************
+Nombre Script            : dbTotalGym
+Description              : Script para crear estructura de la base de datos
+Version	                 : 1.0
+Historia de Modificacion : IS	-	Fecha		-	Descripcion Cambio
+                           ----		----------		----------------------------------------------------------
+                           HECP		20/06/2016		Se creo el script inicial para crear la base de datos
+**************************************************************************************************************/
+
+USE master
+GO
