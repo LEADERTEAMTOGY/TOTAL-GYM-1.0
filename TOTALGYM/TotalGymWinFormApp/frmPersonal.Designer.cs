ca79b543239a6507a3827e064837fc7e93746520
Deysi19 <deysimaria_19@hotmail.com>
2016-06-27 06:57:54 -0500

    se crearon las interfaces

---
+++ TOTALGYM/TotalGymWinFormApp/frmPersonal.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace TotalGymWinFormApp
+{
+    partial class frmPersonal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gppersonal = new System.Windows.Forms.GroupBox();
+            this.btnconsultar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.lblapellido = new System.Windows.Forms.Label();
+            this.lblpuesto = new System.Windows.Forms.Label();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.lblcodigo = new System.Windows.Forms.Label();
+            this.gppersonal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gppersonal
+            // 
+            this.gppersonal.Controls.Add(this.btnconsultar);
+            this.gppersonal.Controls.Add(this.btneliminar);
+            this.gppersonal.Controls.Add(this.textBox4);
+            this.gppersonal.Controls.Add(this.textBox3);
+            this.gppersonal.Controls.Add(this.textBox2);
+            this.gppersonal.Controls.Add(this.textBox1);
+            this.gppersonal.Controls.Add(this.btnagregar);
+            this.gppersonal.Controls.Add(this.lblapellido);
+            this.gppersonal.Controls.Add(this.lblpuesto);
+            this.gppersonal.Controls.Add(this.lblnombre);
+            this.gppersonal.Controls.Add(this.lblcodigo);
+            this.gppersonal.Location = new System.Drawing.Point(12, 31);
+            this.gppersonal.Name = "gppersonal";
+            this.gppersonal.Size = new System.Drawing.Size(506, 274);
+            this.gppersonal.TabIndex = 0;
+            this.gppersonal.TabStop = false;
+            this.gppersonal.Text = "Personal";
+            // 
+            // btnconsultar
+            // 
+            this.btnconsultar.Location = new System.Drawing.Point(364, 43);
+            this.btnconsultar.Name = "btnconsultar";
+            this.btnconsultar.Size = new System.Drawing.Size(96, 35);
+            this.btnconsultar.TabIndex = 10;
+            this.btnconsultar.Text = "Consultar";
+            this.btnconsultar.UseVisualStyleBackColor = true;
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(364, 224);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(96, 37);
+            this.btneliminar.TabIndex = 9;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(84, 180);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(182, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(84, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(252, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(213, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(84, 224);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(89, 37);
+            this.btnagregar.TabIndex = 4;
+            this.btnagregar.Text = "Agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            // 
+            // lblapellido
+            // 
+            this.lblapellido.AutoSize = true;
+            this.lblapellido.Location = new System.Drawing.Point(21, 140);
+            this.lblapellido.Name = "lblapellido";
+            this.lblapellido.Size = new System.Drawing.Size(44, 13);
+            this.lblapellido.TabIndex = 3;
+            this.lblapellido.Text = "Apellido";
+            // 
+            // lblpuesto
+            // 
+            this.lblpuesto.AutoSize = true;
+            this.lblpuesto.Location = new System.Drawing.Point(21, 180);
+            this.lblpuesto.Name = "lblpuesto";
+            this.lblpuesto.Size = new System.Drawing.Size(40, 13);
+            this.lblpuesto.TabIndex = 2;
+            this.lblpuesto.Text = "Puesto";
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(21, 102);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(44, 13);
+            this.lblnombre.TabIndex = 1;
+            this.lblnombre.Text = "Nombre";
+            // 
+            // lblcodigo
+            // 
+            this.lblcodigo.AutoSize = true;
+            this.lblcodigo.Location = new System.Drawing.Point(21, 65);
+            this.lblcodigo.Name = "lblcodigo";
+            this.lblcodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblcodigo.TabIndex = 0;
+            this.lblcodigo.Text = "Codigo";
+            // 
+            // frmPersonal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 317);
+            this.ControlBox = false;
+            this.Controls.Add(this.gppersonal);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPersonal";
+            this.gppersonal.ResumeLayout(false);
+            this.gppersonal.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gppersonal;
+        private System.Windows.Forms.Button btnconsultar;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Label lblapellido;
+        private System.Windows.Forms.Label lblpuesto;
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.Label lblcodigo;
+    }
+}
